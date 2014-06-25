e2d073f0e617d7756a6a9685049d71576f3b5424
Ahmad Azimi <a2azimi@gmail.com>
2014-06-25 21:07:23 +0430

    Add default views
    Add default views.

---
+++ src/views/tinymce4.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>elFinder 2.0</title>
+
+    <!-- jQuery and jQuery UI (REQUIRED) -->
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+
+    <!-- elFinder CSS (REQUIRED) -->
+    <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/elfinder.min.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset($dir.'/css/theme.css') ?>">
+
+    <!-- elFinder JS (REQUIRED) -->
+    <script src="<?= asset($dir.'/js/elfinder.min.js') ?>"></script>
+
+    <?php if($locale){ ?>
+        <!-- elFinder translation (OPTIONAL) -->
+        <script src="<?= asset($dir."/js/i18n/elfinder.$locale.js") ?>"></script>
+    <?php } ?>
+    
+    <!-- elFinder initialization (REQUIRED) -->
+    <script type="text/javascript">
+        var FileBrowserDialogue = {
+            init: function() {
+                // Here goes your code for setting your custom things onLoad.
+            },
+            mySubmit: function (URL) {
+                // pass selected file path to TinyMCE
+                top.tinymce.activeEditor.windowManager.getParams().setUrl(URL);
+
+                // close popup window
+                top.tinymce.activeEditor.windowManager.close();
+            }
+        }
+
+        $().ready(function() {
+            var elf = $('#elfinder').elfinder({
+                // set your elFinder options here
+                <?php if($locale){ ?>
+                    lang: '<?= $locale ?>', // locale
+                <?php } ?>
+                url: '<?= URL::action('Barryvdh\Elfinder\ElfinderController@showConnector') ?>',  // connector URL
+                getFileCallback: function(file) { // editor callback
+                    FileBrowserDialogue.mySubmit(file.url); // pass selected file path to TinyMCE
+                }
+            }).elfinder('instance');
+        });
+    </script>
+</head>
+<body>
+    <!-- Element where elFinder will be created (REQUIRED) -->
+    <div id="elfinder"></div>
+</body>
+</html>
