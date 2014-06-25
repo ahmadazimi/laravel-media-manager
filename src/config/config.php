1e9f457383c45c391a663b3e1ff3e477f7ae2881
Ahmad Azimi <a2azimi@gmail.com>
2014-06-25 21:06:39 +0430

    Config added.
    Config file added.

---
+++ src/config/config.php
@@ -0,0 +1,40 @@
+<?php
+
+return array(
+
+    'upload_dir' => 'media',
+	
+	'customData' => array(),
+	
+	'customHeaders' => array(),
+	
+	/*
+	|----------------------------------------------------------------
+    | AccessControl
+    |----------------------------------------------------------------
+	|
+	| Function or class instance method to control files permissions.
+	| 
+	| https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options#wiki-accessControl
+	|
+	*/
+    'accessControllCallback' => 'W3G\Elfinder\MediaManager::checkAccess',
+	
+	/*
+    |----------------------------------------------------------------
+    | Roots
+    |----------------------------------------------------------------
+    | 
+	| If you don't pass roots property, or pass null to it, the root
+	| file will be LocalFileSystem with the above public upload_dir.
+	| 
+	| If you want custom options or advanced setup, you can set extra 
+	| roots below:
+	|
+	| For more information see
+	| https://github.com/Studio-42/elFinder/wiki/Connector-configuration-options
+    |
+    */
+    'roots' => null,
+
+);
